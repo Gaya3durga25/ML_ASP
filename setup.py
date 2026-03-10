29aec182d6e7d0d07809aad71cfecca4acf16c68
Gaya3durga25 <gaya3durga25@gmail.com>
2026-03-10 12:14:00 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+from typing import List
+
+Hyphen_dot='-e.'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        if Hyphen_dot in requirements:
+            requirements.remove(Hyphen_dot)
+    return requirements
+setup(
+    name="ML_ASP",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+    )
